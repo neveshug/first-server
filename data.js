3128d73918b073b2547b5af89eea836a59d8a031
Hugo Neves <neveshug@gmail.com>
2020-07-13 12:24:26 -0300

    Update datas

---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "starter",
+        name: "Starter",
+        description: "10 Modulos",
+        price: 'Free'
+    },
+    {
+        id: "launchbase",
+        name: "LaunchBase",
+        description: "08 Modulos",
+        price: 'Pay'
+    },
+    {
+        id: "gostack",
+        name: "GoStack",
+        description: "150 Modulos + ",
+        price: 'Pay'
+    }
+]
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Create Server
 const express = require('express')
 const nunjucks = require('nunjucks')
+const courses = require("./data")
 
 const server = express()
 
@@ -17,15 +18,30 @@ nunjucks.configure('views', {
 //Routes
 
 server.get("/", (req, res) => {
-    return res.render('about')
+    const about = {
+        avatar_url: "logo_med_rocketseat.jpg",
+        name: "Rocketseat",
+        description: "Uma breve descrição da empresa, uma breve descrição da empresa ma breve descrição da empresa.",
+        courses: {
+            starter: 'Stater',
+            launchbase: 'Launchbase',
+            gostack: 'GoStack'
+        }
+    }
+    return res.render('about', { about })
 })
 
 server.get("/courses", (req, res) => {
-    return res.render('courses')
+    return res.render('courses', {items : courses})
 })
 
 server.use(function (req, res) {
-    res.status(404).render("not-found");
+    const dataFound = {
+        avatarUrlError : "https://miro.medium.com/max/1170/1*BWXSCXSkkY9juccNWbAfxg.png",
+        codeError : "Erro 404",
+        errorDescription : '“Desligue seu computador..Abra a porta e vá lá fora..Mova suas pernas em qualquer direção.“'
+    }
+    res.status(404).render("not-found", { dataFound });
 })
 
 //Start Server
